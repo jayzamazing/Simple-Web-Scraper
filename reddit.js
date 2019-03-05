2b87e000a28ccc464b91a2213304569dda255a02
Adrian J Lopez <206582563>
2019-03-05 14:09:32 -0500

    Changed reddit to use async await functions.

--- reddit.js
+++ reddit.js
@@ -1,12 +1,25 @@
-const rp = require('request-promise');
+//use pupeteer when dealing with scrapping pages that require javascript execution
+//puppeteer allows control over a headless browser
+const puppeteer = require('puppeteer');
+const $ = require('cheerio');
 const url = 'https://www.reddit.com';
 
-rp(url)
-  .then(function(html){
-    //success!
-    console.log(html);
-  })
-  //error handling code
-  .catch((err) => {
-    console.error(err);
-  });
+//iife
+(
+  async () => {
+    //create browser object
+    const browser = await puppeteer.launch();
+    //get page object
+    const page = await browser.newPage();
+    //have page load the url
+    await page.goto(url);
+    //wait for the content to load
+    const content = await page.content();
+    //iterate through the content looking for h2's
+    $('h2', content).each(function() {
+      console.log($(this).text());
+    });
+    //close the browser
+    await browser.close();
+  }
+)();

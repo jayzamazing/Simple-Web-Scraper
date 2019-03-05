798b59ff70eadd3e567b1b04b0165c624026e084
Adrian J Lopez <206582563>
2019-03-05 12:54:40 -0500

    Taking a look specifically at certain data in page in potusScrapper.

--- potusScraper.js
+++ potusScraper.js
@@ -1,10 +1,13 @@
 const rp = require('request-promise');
+const $ = require('cheerio');
 const url = 'https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States';
 //make request to the url
 rp(url)
   //once response is received successfully
   .then((html) => {
-    console.log(html);
+    // console.log(html);
+    console.log($('big > a', html).length);
+    console.log($('big > a', html));
   })
   .catch((err) => {
     console.log(err);

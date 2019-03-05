ebdd81e33d1356d890e9e5d0749c9c4e918c9733
Adrian J Lopez <206582563>
2019-03-05 13:42:32 -0500

    Changed potusParse into an exportable modules. Consumed module inside of potusscrapper.

--- potusParse.js
+++ potusParse.js
@@ -1,11 +1,19 @@
 const rp = require('request-promise');
-const url = 'https://en.wikipedia.org/wiki/George_Washington';
-//make request to the url
-rp(url)
-    //once response is received successfully
-  .then((html) => {
-    console.log(html);
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+const $ = require('cheerio');
+
+const potusParse = (url) => {
+  //make request to the url
+  return rp(url)
+  //once response is received successfully
+    .then((html) => {
+      return {
+        name: $('.firstHeading', html).text(),
+        birthday: $('.bday', html).text()
+      };
+    })
+    //error handling code
+    .catch((err) => {
+      console.error(err);
+    });
+}
+module.exports = potusParse;
--- potusScraper.js
+++ potusScraper.js
@@ -1,5 +1,6 @@
 const rp = require('request-promise');
 const $ = require('cheerio');
+const potusParse = require('./potusParse');
 const url = 'https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States';
 //make request to the url
 rp(url)
@@ -12,7 +13,15 @@ rp(url)
     for (let i = 0; i < 45; i++) {
       wikiUrls.push($('big > a', html)[i].attribs.href);
     }
-    console.log(wikiUrls);
+    // console.log(wikiUrls);
+    return Promise.all(
+      wikiUrls.map((url) => {
+        return potusParse('https://en.wikipedia.org' + url);
+      })
+    );
+  })
+  .then(function(presidents) {
+    console.log(presidents);
   })
   //error handling code
   .catch((err) => {
